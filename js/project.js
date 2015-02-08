3eba07964a4573ecaaf92c6ff1058231d7b93058
Sam Dav <samatdav@gmail.com>
2015-02-08 10:13:32 +0300

    made items work

--- js/project.js
+++ js/project.js
@@ -1,24 +1,23 @@
 // Establish the array which acts as a data source for the list
 var list = [
+{"type" : "buy", "img" : "img/sun.jpg", "title" : "BDS!", "link" : "http://buydontstudy.ru/","text" : "blah blah"},
+{"type" : "sell", "img" : "img/sun.jpg", "title" : "Hello!", "link" : "#","text" : "blah blah"},
 {"type" : "buy", "img" : "img/sun.jpg", "title" : "Hello!", "link" : "#","text" : "blah blah"},
-{"type" : "buy", "img" : "img/sun.jpg", "title" : "Hello!", "link" : "#","text" : "blah blah"}
+{"type" : "sell", "img" : "img/sun.jpg", "title" : "Hello!", "link" : "#","text" : "blah blah"},
+{"type" : "buy", "img" : "img/sun.jpg", "title" : "Hello!", "link" : "#","text" : "blah blah"},
+{"type" : "sell", "img" : "img/sun.jpg", "title" : "Hello!", "link" : "#","text" : "blah blah"}
 ];
-// var listImg = ['img/sun.jpg', 'img/sun.jpg', 'img/sun.jpg', 'img/sun.jpg', 'img/sun.jpg', 'img/sun.jpg', ]
-// var listHead = ['<a href="">Hello!</a>', '<a href="">Hello!</a>']
-// var listText = ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed', 
-//             'Lorem ipsum dolor sit amet, consectetur adipisi']
-// wait for the page to load
-function makelist(){
-        
 
-           
+function makelist(){
         // Make a container element for the list - which is a <div>
         // You don't actually need this container to make it work
+
+	for( var i =  0 ; i < list.length; ++i){   
+
+        // add it to the page       
         var listContainer = document.createElement("div");
         listContainer.className = "item";
-	for( var i =  0 ; i < list.length; ++i){   
-        // add it to the page
-        document.querySelectorAll(".buy")[0].appendChild(listContainer);
+        document.querySelectorAll('.' + list[i].type)[0].appendChild(listContainer);
 
         // Make the list itself which is a <ul>
         var listElement = document.createElement("ol");
@@ -27,7 +26,11 @@ function makelist(){
         listContainer.appendChild(listElement);
 
   
-		$('ol').append('<li class="item col-md-12"><img class="image" src="' + list[i].img + '" width="160" height="120"/><a href="'+list[i].link+'">'+list[i].title+'</a><p>'+ list[i].text +'</p></li>');
+		$('.' + list[i].type).append(
+		'<li class="item col-md-12" onclick="location.href=\'' + list[i].link + 
+		'\';"><img class="image" src="' + list[i].img + 
+		'" width="90" height="90"/><h3><a href="' + list[i].link + 
+		'">'+list[i].title+'</a></h3><p>'+ list[i].text +'</p></li>');
         }
 
 }
